758ed587ab8f3caf3df10ddec7ab7e694629870a
Ryanhangralim <ryanhangralim@gmail.com>
2024-07-16 15:11:46 +0800

    Add redirect

--- task_1/app/controllers/BulletinController.php
+++ task_1/app/controllers/BulletinController.php
@@ -12,18 +12,14 @@ class BulletinController extends Controller{
         $contentLength = strlen($_POST['content']);
         if ($contentLength > 200 || $contentLength < 10){
             Flasher::setFlash('Your message must be 10 to 200 characters long', 'error');
-            header('Location: ' . BASEURL);
-            exit;
+            Redirect::to(BASEURL);
         } 
         if ($this->model('BulletinModel')->addBulletinMessage($_POST) > 0){
             Flasher::setFlash('Your message has been added', 'success');
-            header('Location: ' . BASEURL);
-            exit;
         } else {
             Flasher::setFlash('Your message failed to be added', 'error');
-            header('Location: ' . BASEURL);
-            exit;
         }
+        Redirect::to(BASEURL);
     }
 }
 
