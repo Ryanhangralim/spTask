18013415409ec988f99b39a480eb1109f6608949
Ryanhangralim <ryanhangralim@gmail.com>
2024-07-16 15:35:08 +0800

    update line

--- task_1/app/core/App.php
+++ task_1/app/core/App.php
@@ -17,7 +17,7 @@ class App{
             }
         }   
 
-        require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
+        require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
         //method
